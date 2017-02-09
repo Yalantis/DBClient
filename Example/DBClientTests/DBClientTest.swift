aab1a00e5af4d89c18bdd510bec6d97fec81d60c
Roman Kyrylenko <roma.kyrylenko@gmail.com>
2017-02-09 14:27:53 +0200

    [Add] created base classes for databases

---
+++ Example/DBClientTests/DBClientTest.swift
@@ -0,0 +1,27 @@
+//
+//  DBClientTest.swift
+//  DBClientTests
+//
+//  Created by Roman Kyrylenko on 2/8/17.
+//  Copyright © 2017 Yalantis. All rights reserved.
+//
+
+import XCTest
+
+class DBClientTest: XCTestCase {
+
+    var expectationTimeout: TimeInterval {
+        return 25
+    }
+    
+    func execute(_ closure: @escaping (XCTestExpectation) -> ()) {
+        let exp = expectation(description: "DBClientTestExpectation")
+        DispatchQueue.global(qos: .background).async {
+            closure(exp)
+        }
+        waitForExpectations(timeout: expectationTimeout) { (error) in
+            XCTAssert(error == nil, "\(error)")
+        }
+    }
+    
+}
---
+++ Example/DBClientTests/Realm/RealmTest.swift
@@ -0,0 +1,9 @@
+//
+//  RealmTest.swift
+//  Example
+//
+//  Created by Roman Kyrylenko on 2/8/17.
+//  Copyright © 2017 Yalantis. All rights reserved.
+//
+
+import Foundation
